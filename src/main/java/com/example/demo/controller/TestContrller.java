a545b058cb1d6c30856147b0f5033b8b8b9e777a
zhoul821706276 <821706276@qq.com>
2022-05-15 22:22:39 +0800

    修改了代码 hot-fix-master888

--- src/main/java/com/example/demo/controller/TestContrller.java
+++ src/main/java/com/example/demo/controller/TestContrller.java
@@ -4,16 +4,7 @@ public class TestContrller {
 
     public static void main(String[] args) {
         System.out.printf("hello word");
-        System.out.println("hello word 2");
-        System.out.println("hello word 3");
-        System.out.println("hot-fix");
-        System.out.println("hot-fix-master");
-        System.out.println("hot-fix-222");
-         System.out.println("hot-fix-3333333333333333333");
-        System.out.println("hot-fix-6666666666");
-         System.out.println("hot-fix-3333333333333333333");
-        System.out.println("hot-fix-6666666666");
-        System.out.println("hot-fix-22288888");
+
 
     }
 }
