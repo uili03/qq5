e6b77097a80bd4501c0f68baf9cee0234b86f9ed
zhoul821706276 <821706276@qq.com>
2022-05-15 21:30:50 +0800

    修改了代码

--- src/main/java/com/example/demo/controller/TestContrller.java
+++ src/main/java/com/example/demo/controller/TestContrller.java
@@ -5,5 +5,6 @@ public class TestContrller {
     public static void main(String[] args) {
         System.out.printf("hello word");
         System.out.println("hello word 2");
+        System.out.println("hello word 3");
     }
 }
